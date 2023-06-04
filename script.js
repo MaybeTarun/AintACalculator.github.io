976cdbeb89b040c2a6ae1a94c1671bc0e104c99b
Tarun Gupta <90061716+MaybeTarun@users.noreply.github.com>
2023-06-04 22:06:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const pun = document.querySelector(".pun")
+var puns = [
+    "The minus key on my calculator is broken, but on the plus side it still works.",
+    "Got stopped by customs with a calculator, an exercise book, and a slide rule. Apparently they are instruments of maths instruction.",
+    "Always trusted my calculator. I can count on it.",
+    "Managed to get some engineering calculations wrong by using the wrong pencil. It wasn’t 2B.",
+    "My calculator stopped working mid way through my engineering exam. I can’t count on it any more.",
+    "Told the detective that it couldn’t have been me as I was using my calculator at the time of the crime. He said, “Well, that adds up”.",
+    "“It’s what’s on the inside that counts”, I said as I explained the chipset of a calculator to a friend.",
+    "Opened the post, and there’s a calculator, an abacus and a letter. Something just doesn’t add up.",
+    "Was going to buy a pocket calculator, then realised I only had a few pockets and could count them myself.",
+    "Farmer wanted to know how many were in his dairy herd, so he used a cowculator."
+]
+
+// function updateText() {
+//     var randomNumber = Math.floor(Math.random() * puns.length);
+//     var newText = puns[randomNumber];
+//     pun.textContent = newText;
+// };
+  
+// window.onload = function() {
+//     setInterval(updateText, 10000);
+// };
